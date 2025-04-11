f07817375b26b0522db3a7f2dc492faea6fdb2be
Maksim Varlyga <maksvarlyga@gmail.com>
2025-04-12 01:05:00 +0300

    feat(dataClasses): added WeightedEdge data class

---
+++ app/src/main/kotlin/model/graphs/dataClasses/WeightedEdge.kt
@@ -0,0 +1,11 @@
+package model.graphs.dataClasses
+
+import model.graphs.interfaces.Edge
+import model.graphs.interfaces.Vertex
+
+data class WeightedEdge<E: Comparable<E>, V: Comparable<V>>(
+    override val id: E,
+    override val from: Vertex<V>,
+    override val to: Vertex<V>,
+    val weight: Int
+): Edge<E, V>
