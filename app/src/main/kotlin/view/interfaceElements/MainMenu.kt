6662934995669248983da5d523186190b7d370c8
Maksim Varlyga <maksvarlyga@gmail.com>
2025-05-07 17:53:01 +0300

    feat(view): added folding animation

--- app/src/main/kotlin/view/interfaceElements/MainMenu.kt
+++ app/src/main/kotlin/view/interfaceElements/MainMenu.kt
@@ -1,11 +1,14 @@
 package view.interfaceElements
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -16,49 +19,65 @@ fun MainMenu(
     isMenuVisible: Boolean,
     onMenuVisibilityChange: (Boolean) -> Unit,
     onNewGraphClick: () -> Unit,
+    modifier: Modifier,
 ) {
-    if (isMenuVisible) {
-        Column(
-            modifier =
-                Modifier
-                    .width(200.dp)
-                    .fillMaxHeight()
-                    .background(Color(0xFFF0F0F0))
-                    .padding(8.dp),
+    Box(
+        modifier = modifier,
+    ) {
+        AnimatedVisibility(
+            visible = isMenuVisible,
+            enter = fadeIn(animationSpec = tween(300)) + slideInHorizontally(animationSpec = tween(300)),
+            exit = fadeOut(animationSpec = tween(300)) + slideOutHorizontally(animationSpec = tween(300)),
         ) {
-            Text("CoreMapX", fontSize = 20.sp, modifier = Modifier.padding(bottom = 16.dp))
-            TextButton(onClick = onNewGraphClick) {
-                Text("New Graph")
-            }
-            TextButton(onClick = { }) {
-                Text("Save Graph")
-            }
-            TextButton(onClick = { }) {
-                Text("Open Graph")
-            }
-            TextButton(onClick = { }) {
-                Text("Analytics")
-            }
-            TextButton(onClick = { }) {
-                Text("Settings")
-            }
-            Spacer(Modifier.weight(1f))
-            TextButton(onClick = { onMenuVisibilityChange(false) }) {
-                Text("<")
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .background(Color(0xFFE6E6FA))
+                        .padding(8.dp),
+            ) {
+                Text("CoreMapX", fontSize = 20.sp, modifier = Modifier.padding(bottom = 16.dp))
+                TextButton(onClick = onNewGraphClick) {
+                    Text("New Graph")
+                }
+                TextButton(onClick = { }) {
+                    Text("Save Graph")
+                }
+                TextButton(onClick = { }) {
+                    Text("Open Graph")
+                }
+                TextButton(onClick = { }) {
+                    Text("Analytics")
+                }
+                TextButton(onClick = { }) {
+                    Text("Settings")
+                }
+                Spacer(Modifier.weight(1f))
+                TextButton(onClick = { onMenuVisibilityChange(false) }) {
+                    Text("<")
+                }
             }
         }
-    } else {
-        Column(
-            modifier =
-                Modifier
-                    .width(50.dp)
-                    .fillMaxHeight(),
-            verticalArrangement = Arrangement.Center,
+
+        AnimatedVisibility(
+            visible = !isMenuVisible,
+            enter = fadeIn(animationSpec = tween(300)) + slideInHorizontally(animationSpec = tween(300)),
+            exit = fadeOut(animationSpec = tween(300)) + slideOutHorizontally(animationSpec = tween(300)),
         ) {
-            Button(
-                onClick = { onMenuVisibilityChange(true) },
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxHeight()
+                        .padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(">")
+                Button(
+                    onClick = { onMenuVisibilityChange(true) },
+                ) {
+                    Text(">")
+                }
             }
         }
     }
