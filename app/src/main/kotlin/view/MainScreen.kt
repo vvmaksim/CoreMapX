14823a916fc369f0c642b8c25fb5c334eb908541
Maksim Varlyga <maksvarlyga@gmail.com>
2025-05-07 17:55:15 +0300

    fix(view): fixed bug with overlapping objects on main menu

--- app/src/main/kotlin/view/MainScreen.kt
+++ app/src/main/kotlin/view/MainScreen.kt
@@ -2,14 +2,16 @@ package view
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import model.graphs.classes.DirectedUnweightedGraph
 import model.graphs.classes.DirectedWeightedGraph
 import model.graphs.classes.UndirectedUnweightedGraph
 import model.graphs.classes.UndirectedWeightedGraph
 import model.graphs.interfaces.Graph
 import view.interfaceElements.MainMenu
-import view.interfaceElements.MainWorkArea
+import view.interfaceElements.WorkSpace
 import view.interfaceElements.dialogs.NewGraph
 import viewmodel.MainScreenViewModel
 
@@ -18,18 +20,25 @@ fun <E : Comparable<E>, V : Comparable<V>> MainScreen(viewModel: MainScreenViewM
     var isMenuVisible by remember { mutableStateOf(true) }
     var showNewGraphDialog by remember { mutableStateOf(false) }
 
-    Row(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        WorkSpace(
+            viewModel,
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(start = 200.dp),
+        )
+
         MainMenu(
             isMenuVisible = isMenuVisible,
             onMenuVisibilityChange = { isMenuVisible = it },
             onNewGraphClick = { showNewGraphDialog = true },
+            modifier =
+                Modifier
+                    .fillMaxHeight()
+                    .width(200.dp)
+                    .align(Alignment.CenterStart),
         )
-        if (viewModel.isGraphActive) {
-            MainWorkArea(
-                viewModel,
-                modifier = Modifier.weight(1f),
-            )
-        }
     }
 
     if (showNewGraphDialog) {
