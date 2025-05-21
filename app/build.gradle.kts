299a04fa9b3a77cb0ed7cf7b90662756d89db8e8
Maksim Varlyga <maksvarlyga@gmail.com>
2025-05-21 20:08:47 +0300

    feat(build.gradle.kts): added plagin and library for json serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("buildlogic.kotlin-application-conventions")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 dependencies {
@@ -8,6 +9,7 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.compose.material.icons.core)
     implementation(libs.compose.material.icons.extended)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation(project(":utilities"))
     testImplementation(kotlin("test"))
 }
