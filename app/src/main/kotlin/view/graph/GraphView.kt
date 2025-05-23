8e35292a286d60c09b762c0ae62d2b4d44499884
Maksim Varlyga <maksvarlyga@gmail.com>
2025-05-23 16:57:57 +0300

    fix(view): fixed graph drag area

--- app/src/main/kotlin/view/graph/GraphView.kt
+++ app/src/main/kotlin/view/graph/GraphView.kt
@@ -4,14 +4,8 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventType
@@ -29,9 +23,7 @@ fun <E : Comparable<E>, V : Comparable<V>> GraphView(
     onPan: (Float, Float) -> Unit,
     onZoom: (Float) -> Unit,
 ) {
-    var dragOffset by remember { mutableStateOf(Offset.Zero) }
-    val canvasDragSize = (config.getIntValue("canvasDragSize") ?: 0).dp
-    val canvasDragRatio = config.getDoubleValue("canvasDragRatio") ?: 0.0
+    val canvasDragRatio = (config.getIntValue("canvasDragRatio") ?: 0)
     Box(
         modifier =
             Modifier
@@ -46,28 +38,26 @@ fun <E : Comparable<E>, V : Comparable<V>> GraphView(
                             }
                         }
                     }
+                }
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDrag = { change, dragAmount ->
+                            change.consume()
+                            onPan(dragAmount.x * canvasDragRatio, dragAmount.y * canvasDragRatio)
+                        }
+                    )
                 },
     ) {
         Box(
             modifier =
                 Modifier
-                    .size(canvasDragSize, canvasDragSize)
-                    .offset(
-                        (offsetX + (dragOffset.x * canvasDragRatio)).dp,
-                        (offsetY + (dragOffset.y * canvasDragRatio)).dp,
-                    ).graphicsLayer(
+                    .fillMaxSize()
+                    .offset(offsetX.dp,offsetY.dp)
+                    .graphicsLayer(
                         scaleX = scale,
                         scaleY = scale,
                         transformOrigin = TransformOrigin(0.5f, 0.5f),
-                    ).pointerInput(Unit) {
-                        detectDragGestures(
-                            onDrag = { change, dragAmount ->
-                                change.consume()
-                                dragOffset += dragAmount
-                                onPan(dragAmount.x, dragAmount.y)
-                            },
-                        )
-                    },
+                    )
         ) {
             viewModel.vertices.forEach { v ->
                 VertexView(v, Modifier)
