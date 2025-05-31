df20a5b38f769a74a5e2e8075cf60a5bb6b65624
Maksim Varlyga <maksvarlyga@gmail.com>
2025-05-31 03:47:01 +0300

    feat(view): added hovered effect for VertexView

--- app/src/main/kotlin/view/graph/VertexView.kt
+++ app/src/main/kotlin/view/graph/VertexView.kt
@@ -6,43 +6,66 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.coremapx.app.config
 import viewmodel.graph.VertexViewModel
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun <V : Comparable<V>> VertexView(
     viewModel: VertexViewModel<V>,
     modifier: Modifier = Modifier,
+    hoveredBorderColor: Color = Color.Gray,
 ) {
     val vertexLabelColor = config.getColor("vertexLabelColor")
     val vertexLabelSize = (config.getIntValue("vertexLabelSize") ?: 0).sp
+    var isHovered by remember { mutableStateOf(false) }
 
     Box(
-        modifier =
-            modifier
-                .size(viewModel.radius * 2, viewModel.radius * 2)
-                .offset(viewModel.x, viewModel.y)
-                .background(
-                    color = viewModel.color,
-                    shape = CircleShape,
-                ).pointerInput(viewModel) {
-                    detectDragGestures { change, dragAmount ->
-                        change.consume()
-                        viewModel.onDrag(dragAmount)
-                    }
-                },
+        modifier = modifier
+            .size(viewModel.radius * 2, viewModel.radius * 2)
+            .offset(viewModel.x, viewModel.y)
+            .background(
+                color = viewModel.color,
+                shape = CircleShape,
+            )
+            .drawBehind {
+                if (isHovered) {
+                    drawCircle(
+                        color = hoveredBorderColor,
+                        style = Stroke(width = 3f),
+                        radius = viewModel.radius.toPx()
+                    )
+                }
+            }
+            .onPointerEvent(PointerEventType.Enter) { isHovered = true }
+            .onPointerEvent(PointerEventType.Exit) { isHovered = false }
+            .pointerInput(viewModel) {
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    viewModel.onDrag(dragAmount)
+                }
+            }
     ) {
         if (viewModel.labelVisible) {
             Text(
-                modifier =
-                    Modifier
-                        .align(Alignment.Center)
-                        .offset(0.dp, -viewModel.radius - 10.dp),
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .offset(0.dp, -viewModel.radius - 10.dp),
                 text = viewModel.label,
                 color = vertexLabelColor,
                 fontSize = vertexLabelSize,
