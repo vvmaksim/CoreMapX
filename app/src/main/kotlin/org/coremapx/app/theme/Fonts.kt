74c36de3f166884229f6d4b6c5eec7d8f254d3fe
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-06 09:00:35 +0300

    feat(theme): added Fonts.kt

---
+++ app/src/main/kotlin/org/coremapx/app/theme/Fonts.kt
@@ -0,0 +1,15 @@
+package org.coremapx.app.theme
+
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+
+object Fonts {
+    val rubik =
+        FontFamily(
+            Font("fonts/Rubik-Regular.ttf", FontWeight.Normal),
+            Font("fonts/Rubik-Bold.ttf", FontWeight.Bold),
+            Font("fonts/Rubik-Medium.ttf", FontWeight.Medium),
+            Font("fonts/Rubik-Light.ttf", FontWeight.Light),
+        )
+}
