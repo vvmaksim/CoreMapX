9ce553126914369daae06c409b7cb9c4a766e6ca
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-10 07:12:16 +0300

    feat(userDirectory): updated ConfigRopository class

--- app/src/main/kotlin/org/coremapx/app/userDirectory/ConfigRepository.kt
+++ app/src/main/kotlin/org/coremapx/app/userDirectory/ConfigRepository.kt
@@ -2,6 +2,9 @@ package org.coremapx.app.userDirectory
 
 import androidx.compose.ui.graphics.Color
 import extensions.toColorOrNull
+import model.result.ConfigErrors
+import model.result.FileErrors
+import model.result.Result
 import model.result.showConfigErrorDialog
 import mu.KotlinLogging
 import java.io.File
@@ -18,10 +21,14 @@ class ConfigRepository {
     private val privateConfig: MutableMap<String, String> = mutableMapOf()
     private val defaultConfig: MutableMap<String, String> = mutableMapOf()
 
+    var states: ConfigStates
+        private set
+
     init {
         loadUserConfig()
         loadPrivateConfig()
         validateUserConfig()
+        states = ConfigStates(this)
     }
 
     fun getStringValue(key: String): String =
@@ -33,12 +40,6 @@ class ConfigRepository {
     fun getIntValue(key: String): Int =
         getStringValue(key).toIntOrNull() ?: throw IllegalArgumentException("Invalid key:$key. Value must be Int type")
 
-    fun getLongValue(key: String): Long =
-        getStringValue(key).toLongOrNull() ?: throw IllegalArgumentException("Invalid key:$key. Value must be Long type")
-
-    fun getDoubleValue(key: String): Double =
-        getStringValue(key).toDoubleOrNull() ?: throw IllegalArgumentException("Invalid key:$key. Value must be Double type")
-
     fun getFloatValue(key: String): Float =
         getStringValue(key).toFloatOrNull() ?: throw IllegalArgumentException("Invalid key:$key. Value must be Float type")
 
@@ -47,20 +48,26 @@ class ConfigRepository {
     fun setValue(
         key: String,
         value: String,
-    ) {
-        updateConfigFile(key, value)
+    ): Result<Boolean> {
+        val validateResult = ConfigValidator.validate(key, value)
+        if (validateResult is Result.Error) return validateResult
+        val updateConfigFileResult = updateConfigFile(key, value)
+        if (updateConfigFileResult is Result.Error) return updateConfigFileResult
         userConfig[key] = value
+        states.updateValue(key, value)
         logger.info { "Updated config. For key: $key new value: $value" }
+        return Result.Success(true)
     }
 
     private fun updateConfigFile(
         key: String,
         value: String,
-    ) {
+    ): Result<Boolean> {
         val configFile = File(mainConfigPath)
         if (!configFile.exists()) {
-            logger.error { "There is nothing to update, the configuration file has not been found" }
-            return
+            val message = "There is nothing to update, the configuration file has not been found"
+            logger.error { message }
+            return Result.Error(FileErrors.ErrorReadingFile(message))
         }
         val lines = configFile.readLines()
         val updatedLines =
@@ -73,7 +80,7 @@ class ConfigRepository {
             }
         if (lines == updatedLines) {
             logger.warn { "Key '$key' not found in config file" }
-            return
+            return Result.Error(ConfigErrors.UnknownProperty(key))
         }
         configFile.bufferedWriter().use { writer ->
             updatedLines.forEachIndexed { index, line ->
@@ -83,6 +90,7 @@ class ConfigRepository {
                 }
             }
         }
+        return Result.Success(true)
     }
 
     private fun loadUserConfig() {
@@ -119,7 +127,7 @@ class ConfigRepository {
         defaultConfig.forEach { param ->
             if (!userConfig.containsKey(param.key)) missingParameters.add(param.key)
         }
-        if (missingParameters.isNotEmpty()) showConfigErrorDialog("Missing parameters in config: $missingParameters")
+        if (missingParameters.isNotEmpty()) throw IllegalArgumentException("Missing parameters in config: $missingParameters")
     }
 
     private fun getColorErrorMessage(propertyName: String): String = "$propertyName must be color in hex format. For example `...=#FFFFFF`"
@@ -127,14 +135,7 @@ class ConfigRepository {
     private fun validateUserConfig() {
         try {
             comparisonWithDefaultConfig()
-            checkGeneralSettings()
-            checkColorsSettings()
-            checkMainScreenSettings()
-            checkMainMenuSettings()
-            checkTitleBarSettings()
-            checkCommandFieldSettings()
-            checkWorkAreaSettings()
-            checkPerformanceSettings()
+            validateUserConfigValues()
             logger.info { "Config has been loaded successfully" }
         } catch (ex: IllegalArgumentException) {
             logger.error { "Config validation failed" }
@@ -142,82 +143,12 @@ class ConfigRepository {
         }
     }
 
-    private fun validateColor(value: String): Boolean = value.toColorOrNull() != null
-
-    private fun checkGeneralSettings() {
-        require(getStringValue("language") in listOf("ru", "en")) { "Supported languages: en, ru" }
-        require(getStringValue("theme") in listOf("light", "dark")) { "Supported themes: light, dark" }
-        require(getStringValue("fileDialogTheme") in listOf("light", "dark")) { "Supported fileDialogTheme: light, dark" }
-    }
-
-    private fun checkColorsSettings() {
-        require(validateColor(getStringValue("primary"))) { getColorErrorMessage("primary") }
-        require(validateColor(getStringValue("primaryVariant"))) { getColorErrorMessage("primaryVariant") }
-        require(validateColor(getStringValue("secondary"))) { getColorErrorMessage("secondary") }
-        require(validateColor(getStringValue("secondaryVariant"))) { getColorErrorMessage("secondaryVariant") }
-        require(validateColor(getStringValue("background"))) { getColorErrorMessage("background") }
-        require(validateColor(getStringValue("surface"))) { getColorErrorMessage("surface") }
-        require(validateColor(getStringValue("error"))) { getColorErrorMessage("error") }
-        require(validateColor(getStringValue("onPrimary"))) { getColorErrorMessage("onPrimary") }
-        require(validateColor(getStringValue("onSecondary"))) { getColorErrorMessage("onSecondary") }
-        require(validateColor(getStringValue("onBackground"))) { getColorErrorMessage("onBackground") }
-        require(validateColor(getStringValue("onSurface"))) { getColorErrorMessage("onSurface") }
-        require(validateColor(getStringValue("onError"))) { getColorErrorMessage("onError") }
-
-        require(validateColor(getStringValue("borderColor"))) { getColorErrorMessage("borderColor") }
-        require(validateColor(getStringValue("cancelIconColor"))) { getColorErrorMessage("cancelIconColor") }
-        require(validateColor(getStringValue("warningColor"))) { getColorErrorMessage("warningColor") }
-        require(validateColor(getStringValue("vertexMainColor"))) { getColorErrorMessage("vertexMainColor") }
-        require(validateColor(getStringValue("hoveredBorderColor"))) { getColorErrorMessage("hoveredBorderColor") }
-        require(validateColor(getStringValue("edgeMainColor"))) { getColorErrorMessage("edgeMainColor") }
-        require(validateColor(getStringValue("canvasBackgroundColor"))) { getColorErrorMessage("canvasBackgroundColor") }
-        require(validateColor(getStringValue("commandLineBackgroundColor"))) { getColorErrorMessage("commandLineBackgroundColor") }
-    }
-
-    private fun checkMainScreenSettings() {
-        require((((getIntValue("mainScreenStartHeight")) >= 720))) { "mainScreenStartHeight must be >= 720 px" }
-        require((((getIntValue("mainScreenStartWidth")) >= 1280))) { "mainScreenStartWidth must be >= 1280 px" }
-        require(getStringValue("startWindowPlacement") in listOf("FullScreen", "Floating", "Maximized")) {
-            "Supported startWindowPlacement values: FullScreen, Floating, Maximized"
+    private fun validateUserConfigValues() {
+        userConfig.forEach { (key, value) ->
+            val validateResult = ConfigValidator.validate(key, value)
+            if (validateResult is Result.Error) {
+                throw IllegalArgumentException("ERROR: ${validateResult.error.type}.${validateResult.error.description}")
+            }
         }
-    }
-
-    private fun checkMainMenuSettings() {
-        require((((getIntValue("mainMenuWidth")) >= 200))) { "mainMenuWidth must be >= 200 dp" }
-    }
-
-    private fun checkTitleBarSettings() {
-        require((((getIntValue("titleBarHeight")) >= 35))) { "titleBarHeight must be >= 35 dp" }
-        require((((getIntValue("titleBarIconSize")) >= 16))) { "titleBarIconSize must be >= 16 dp" }
-    }
-
-    private fun checkCommandFieldSettings() {
-        require((((getIntValue("messageOutputHeight")) >= 150))) { "messageOutputHeight must be >= 150 dp" }
-        val maxCountMessages = getIntValue("maxCountMessages")
-        require((1 <= maxCountMessages) && (maxCountMessages <= 10000)) { "maxCountMessages must be >= 1, but <= 10000" }
-        require((((getIntValue("commandFieldWidth")) >= 400))) { "commandFieldWidth must be >= 400 dp" }
-    }
-
-    private fun checkWorkAreaSettings() {
-        require((((getIntValue("graphLayoutHeight")) >= 2000))) { "graphLayoutHeight must be >= 2000 dp" }
-        require((((getIntValue("graphLayoutWidth")) >= 1000))) { "graphLayoutWidth must be >= 1000 dp" }
-        require((((getIntValue("vertexRadius")) >= 1))) { "vertexRadius must be >= 1 dp" }
-        require((((getIntValue("vertexLabelSize")) >= 1))) { "vertexLabelSize must be >= 1 sp" }
-        require((((getIntValue("edgeLabelSize")) >= 1))) { "edgeLabelSize must be >= 1 sp" }
-        val edgeArrowSize = getIntValue("edgeArrowSize")
-        require((1 <= edgeArrowSize) && (edgeArrowSize <= 100)) { "edgeArrowSize must be >= 1, but <= 100" }
-        require((((getIntValue("edgeWidth")) >= 1))) { "edgeWidth must be >= 1 dp" }
-        val canvasDragRatio = getDoubleValue("canvasDragRatio")
-        require((0.1 <= canvasDragRatio) && (canvasDragRatio <= 10)) { "canvasDragRatio must be >= 0.1, but <= 10" }
-        require((((getIntValue("canvasLimit")) >= 2000))) { "canvasLimit must be >= 2000 px" }
-    }
-
-    private fun checkPerformanceSettings() {
-        val animationDuration = getIntValue("animationDuration")
-        require((100 <= animationDuration) && (animationDuration <= 1500)) { "animationDuration must be >= 100, but <= 1500 ms" }
-        val commandFieldScrollDelay = getIntValue("commandFieldScrollDelay")
-        require(
-            (10 <= commandFieldScrollDelay) && (commandFieldScrollDelay <= 300),
-        ) { "commandFieldScrollDelay must be >= 10, but <= 300 ms" }
     }
 }
