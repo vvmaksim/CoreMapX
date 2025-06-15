0b473af47c070d7cd5608ddf65399b9b6b3188e5
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-15 10:25:59 +0300

    feat(view): added @Preview functions in Logo.kt

--- app/src/main/kotlin/view/appInterface/Logo.kt
+++ app/src/main/kotlin/view/appInterface/Logo.kt
@@ -1,9 +1,11 @@
 package view.appInterface
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -13,6 +15,8 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.coremapx.app.theme.AppTheme
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Suppress("ktlint:standard:function-naming")
@@ -46,6 +50,32 @@ fun Logo(
                     .align(Alignment.Center),
             contentScale = contentScale,
             colorFilter = ColorFilter.tint(contentColor),
+        )
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview
+@Composable
+fun PreviewLogo1() {
+    AppTheme {
+        Logo(
+            size = 500.dp,
+            backgroundColor = MaterialTheme.colors.onSurface,
+            contentColor = MaterialTheme.colors.background,
+        )
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview
+@Composable
+fun PreviewLogo2() {
+    AppTheme {
+        Logo(
+            size = 500.dp,
+            backgroundColor = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.onSurface,
         )
     }
 }
