02b8df6755e4c27b2edfc18c9cb7acbd0fc9a1f4
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-18 23:24:49 +0300

    feat(view): added SwitchLine.kt

---
+++ app/src/main/kotlin/view/appInterface/settingsElements/lines/SwitchLine.kt
@@ -0,0 +1,115 @@
+package view.appInterface.settingsElements.lines
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.coremapx.app.theme.AppTheme
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun SettingsSwitch(
+    title: String,
+    description: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    isExpanded: Boolean = false,
+) {
+    var expanded by remember { mutableStateOf(isExpanded) }
+
+    Column(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.Companion.CenterVertically,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier.weight(1f),
+            )
+            Switch(
+                checked = checked,
+                onCheckedChange = onCheckedChange,
+                colors = SwitchDefaults.colors(checkedThumbColor = MaterialTheme.colors.primary),
+            )
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Default.ExpandLess else Icons.Default.ExpandMore,
+                    contentDescription = if (expanded) "Hide description" else "Show description",
+                    tint = MaterialTheme.colors.primary,
+                )
+            }
+        }
+        if (expanded) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.6f),
+                modifier = Modifier.padding(horizontal = 16.dp),
+            )
+        }
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview
+@Composable
+private fun PreviewSettingsSwitch() {
+    AppTheme {
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+            ) {
+                SettingsSwitch(
+                    title = "Setting 1",
+                    description = "Some description",
+                    checked = true,
+                    onCheckedChange = {},
+                    modifier = Modifier.padding(16.dp),
+                    isExpanded = true,
+                )
+                Spacer(Modifier.height(8.dp))
+                SettingsSwitch(
+                    title = "Setting 2",
+                    description = "Some description",
+                    checked = false,
+                    onCheckedChange = {},
+                    modifier = Modifier.padding(16.dp),
+                    isExpanded = false,
+                )
+            }
+        }
+    }
+}
