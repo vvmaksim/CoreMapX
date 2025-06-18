4a7b1ff6f8360cfaccbbd127b3c3435752e06f90
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-18 23:27:32 +0300

    feat(view): added TitleBarBlock.kt

---
+++ app/src/main/kotlin/view/appInterface/settingsElements/blocks/TitleBarBlock.kt
@@ -0,0 +1,60 @@
+package view.appInterface.settingsElements.blocks
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import org.coremapx.app.config
+import org.coremapx.app.theme.AppTheme
+import view.appInterface.settingsElements.lines.NumberTextFieldLine
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun TitleBarBlock() {
+    val titleBarHeight by remember { config.states.titleBarHeight }
+    val titleBarIconSize by remember { config.states.titleBarIconSize }
+
+    Column {
+        NumberTextFieldLine(
+            title = "Title bar height",
+            valueType = Int::class,
+            value = TextFieldValue("$titleBarHeight"),
+            onValueChange = {
+                config.setValue("titleBarHeight", it.text)
+            },
+        )
+        Spacer(Modifier.height(8.dp))
+        NumberTextFieldLine(
+            title = "Title bar icon size",
+            valueType = Int::class,
+            value = TextFieldValue("$titleBarIconSize"),
+            onValueChange = {
+                config.setValue("titleBarIconSize", it.text)
+            },
+        )
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview
+@Composable
+private fun PreviewTitleBarBlock() {
+    AppTheme {
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.padding(8.dp),
+            color = MaterialTheme.colors.background,
+        ) {
+            TitleBarBlock()
+        }
+    }
+}
