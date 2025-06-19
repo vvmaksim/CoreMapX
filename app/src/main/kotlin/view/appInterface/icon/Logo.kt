cad092e15a135d05ee57cf2221745f1c54cea24e
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 11:27:31 +0300

    fix(view): updated @Preview function in Logo.kt by PreviewSurface

--- app/src/main/kotlin/view/appInterface/icon/Logo.kt
+++ app/src/main/kotlin/view/appInterface/icon/Logo.kt
@@ -3,7 +3,10 @@ package view.appInterface.icon
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -17,6 +20,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.theme.AppTheme
+import view.appInterface.preview.PreviewSurface
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Suppress("ktlint:standard:function-naming")
@@ -57,25 +61,26 @@ fun Logo(
 @Suppress("ktlint:standard:function-naming")
 @Preview
 @Composable
-private fun PreviewLogo1() {
+private fun PreviewLogo() {
     AppTheme {
-        Logo(
-            size = 500.dp,
-            backgroundColor = MaterialTheme.colors.onSurface,
-            contentColor = MaterialTheme.colors.background,
-        )
-    }
-}
-
-@Suppress("ktlint:standard:function-naming")
-@Preview
-@Composable
-private fun PreviewLogo2() {
-    AppTheme {
-        Logo(
-            size = 500.dp,
-            backgroundColor = MaterialTheme.colors.background,
-            contentColor = MaterialTheme.colors.onSurface,
+        PreviewSurface(
+            content = {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                ) {
+                    Logo(
+                        size = 300.dp,
+                        backgroundColor = MaterialTheme.colors.onSurface,
+                        contentColor = MaterialTheme.colors.background,
+                    )
+                    Spacer(Modifier.padding(8.dp))
+                    Logo(
+                        size = 300.dp,
+                        backgroundColor = MaterialTheme.colors.background,
+                        contentColor = MaterialTheme.colors.onSurface,
+                    )
+                }
+            },
         )
     }
 }
