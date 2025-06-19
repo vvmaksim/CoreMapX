3d8cf6d665d02b09749435d674b37b08db9c5786
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 06:08:51 +0300

    feat(view): added SettingsDescriptionIconButton.kt

---
+++ app/src/main/kotlin/view/appInterface/settingsElements/description/SettingsDescriptionIconButton.kt
@@ -0,0 +1,64 @@
+package view.appInterface.settingsElements.description
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.coremapx.app.theme.AppTheme
+
+@Suppress("ktlint:standard:function-naming")
+@Composable
+fun SettingsDescriptionIconButton(
+    onClick: () -> Unit,
+    isExpanded: Boolean,
+    iconTintColor: Color = MaterialTheme.colors.primary,
+    modifier: Modifier = Modifier,
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (isExpanded) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
+            contentDescription = if (isExpanded) "Hide description" else "Show description",
+            tint = iconTintColor,
+            modifier = modifier,
+        )
+    }
+}
+
+@Suppress("ktlint:standard:function-naming")
+@Preview
+@Composable
+private fun PreviewSettingsDescriptionIconButton() {
+    AppTheme {
+        Surface(
+            shape = MaterialTheme.shapes.large,
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.padding(8.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+            ) {
+                SettingsDescriptionIconButton(
+                    onClick = {},
+                    isExpanded = true,
+                )
+                Spacer(Modifier.height(8.dp))
+                SettingsDescriptionIconButton(
+                    onClick = {},
+                    isExpanded = false,
+                )
+            }
+        }
+    }
+}
