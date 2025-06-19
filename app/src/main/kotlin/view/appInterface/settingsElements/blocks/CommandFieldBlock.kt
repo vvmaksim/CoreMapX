d2a4f8908ba512b4cd12b8ba55afeba64f470076
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 11:28:16 +0300

    fix(view): updated @Preview function in CommandFieldBlock.kt by PreviewSurface

--- app/src/main/kotlin/view/appInterface/settingsElements/blocks/CommandFieldBlock.kt
+++ app/src/main/kotlin/view/appInterface/settingsElements/blocks/CommandFieldBlock.kt
@@ -4,9 +4,6 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -15,6 +12,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.config
 import org.coremapx.app.theme.AppTheme
+import view.appInterface.preview.PreviewSurface
 import view.appInterface.settingsElements.lines.NumberTextFieldLine
 import view.appInterface.settingsElements.lines.SwitchLine
 
@@ -86,12 +84,6 @@ fun CommandFieldBlock(isExpandedSettings: Boolean = config.states.isExpandedSett
 @Composable
 private fun PreviewCommandFieldBlock() {
     AppTheme {
-        Surface(
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier.padding(8.dp),
-            color = MaterialTheme.colors.background,
-        ) {
-            CommandFieldBlock()
-        }
+        PreviewSurface(content = { CommandFieldBlock() })
     }
 }
