658ba8b21c61799be0d4709b592650499f56f4a4
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 11:27:53 +0300

    fix(view): updated @Preview function in ColorsBlock.kt by PreviewSurface

--- app/src/main/kotlin/view/appInterface/settingsElements/blocks/ColorsBlock.kt
+++ app/src/main/kotlin/view/appInterface/settingsElements/blocks/ColorsBlock.kt
@@ -5,9 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -15,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.config
 import org.coremapx.app.theme.AppTheme
+import view.appInterface.preview.PreviewSurface
 import view.appInterface.settingsElements.lines.ColorPickLine
 
 @Suppress("ktlint:standard:function-naming")
@@ -354,12 +352,6 @@ fun ColorsBlock(isExpandedSettings: Boolean = config.states.isExpandedSettings.v
 @Composable
 private fun PreviewColorsBlock() {
     AppTheme {
-        Surface(
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier.padding(8.dp),
-            color = MaterialTheme.colors.background,
-        ) {
-            ColorsBlock()
-        }
+        PreviewSurface(content = { ColorsBlock() })
     }
 }
