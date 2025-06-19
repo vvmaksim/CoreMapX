962eb54ffb765efb0dfb1d8cd0f5b2015fd1c2ce
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 11:30:06 +0300

    fix(view): updated @Preview function in WorkAreaBlock.kt by PreviewSurface

--- app/src/main/kotlin/view/appInterface/settingsElements/blocks/WorkAreaBlock.kt
+++ app/src/main/kotlin/view/appInterface/settingsElements/blocks/WorkAreaBlock.kt
@@ -4,9 +4,6 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -15,6 +12,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.config
 import org.coremapx.app.theme.AppTheme
+import view.appInterface.preview.PreviewSurface
 import view.appInterface.settingsElements.lines.NumberTextFieldLine
 
 @Suppress("ktlint:standard:function-naming")
@@ -146,12 +144,6 @@ fun WorkAreaBlock(isExpandedSettings: Boolean = config.states.isExpandedSettings
 @Composable
 private fun PreviewWorkAreaBlock() {
     AppTheme {
-        Surface(
-            shape = MaterialTheme.shapes.large,
-            modifier = Modifier.padding(8.dp),
-            color = MaterialTheme.colors.background,
-        ) {
-            WorkAreaBlock()
-        }
+        PreviewSurface(content = { WorkAreaBlock() })
     }
 }
