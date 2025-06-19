923ecd90db509a7dc439fdb44802a5eddd12ca33
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 05:10:38 +0300

    fix(view): replaced DropdownSelectButton on SwitchLine in CommandFieldBlock.kt

--- app/src/main/kotlin/view/appInterface/settingsElements/blocks/CommandFieldBlock.kt
+++ app/src/main/kotlin/view/appInterface/settingsElements/blocks/CommandFieldBlock.kt
@@ -3,7 +3,6 @@ package view.appInterface.settingsElements.blocks
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
@@ -16,9 +15,8 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.config
 import org.coremapx.app.theme.AppTheme
-import view.appInterface.settingsElements.lines.DropdownSelectLine
 import view.appInterface.settingsElements.lines.NumberTextFieldLine
-import java.util.Locale.getDefault
+import view.appInterface.settingsElements.lines.SwitchLine
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
@@ -28,10 +26,6 @@ fun CommandFieldBlock() {
     val commandFieldWidth by remember { config.states.commandFieldWidth }
     val isTransparentCommandLine by remember { config.states.isTransparentCommandLine }
 
-    val dropdownSelectButtonModifier =
-        Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
     Column {
         NumberTextFieldLine(
             title = "Message output height",
@@ -60,23 +54,17 @@ fun CommandFieldBlock() {
             },
         )
         Spacer(Modifier.height(8.dp))
-        DropdownSelectLine(
+        SwitchLine(
             title = "Command line is transparent",
-            items = listOf("True", "False"),
-            selectedItem =
-                isTransparentCommandLine.toString().replaceFirstChar {
-                    if (it.isLowerCase()) {
-                        it.titlecase(
-                            getDefault(),
-                        )
-                    } else {
-                        it.toString()
-                    }
-                },
-            onItemSelected = {
-                config.setValue("isTransparentCommandLine", it.lowercase())
-            },
-            modifier = dropdownSelectButtonModifier,
+            description =
+                """
+                This parameter is responsible for the transparency of the command line background.
+
+                If it is enabled, the background of the text field will be transparent.
+                If it is turned off, the background of the text field will take on the color specified in the corresponding parameter in the color settings.
+                """.trimIndent(),
+            checked = isTransparentCommandLine,
+            onCheckedChange = { config.setValue("isTransparentCommandLine", it.toString()) },
         )
     }
 }
