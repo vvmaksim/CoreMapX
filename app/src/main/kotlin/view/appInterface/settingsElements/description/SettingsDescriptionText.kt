3613c65a83e339ce0b4d0db3837a07d100670dd7
Maksim Varlyga <maksvarlyga@gmail.com>
2025-06-19 11:30:56 +0300

    fix(view): updated @Preview function in SettingsDescriptionText.kt by PreviewSurface

--- app/src/main/kotlin/view/appInterface/settingsElements/description/SettingsDescriptionText.kt
+++ app/src/main/kotlin/view/appInterface/settingsElements/description/SettingsDescriptionText.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import org.coremapx.app.theme.AppTheme
+import view.appInterface.preview.PreviewSurface
 
 @Suppress("ktlint:standard:function-naming")
 @Composable
@@ -36,32 +36,30 @@ fun SettingsDescriptionText(
 @Composable
 private fun PreviewSettingsDescriptionText() {
     AppTheme {
-        Surface(
-            shape = MaterialTheme.shapes.large,
-            color = MaterialTheme.colors.background,
-            modifier = Modifier.padding(8.dp),
-        ) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-            ) {
-                SettingsDescriptionText(
-                    description = "Some description",
-                )
-                Spacer(Modifier.height(8.dp))
-                SettingsDescriptionText(
-                    description =
-                        """
-                        Some long description
-                        Some long description
-                        Some long description
-                        Some long description
-                        Some long description
-                        Some long description
-                        Some long description
-                        Some long description
-                        """.trimIndent(),
-                )
-            }
-        }
+        PreviewSurface(
+            content = {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                ) {
+                    SettingsDescriptionText(
+                        description = "Some description",
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    SettingsDescriptionText(
+                        description =
+                            """
+                            Some long description
+                            Some long description
+                            Some long description
+                            Some long description
+                            Some long description
+                            Some long description
+                            Some long description
+                            Some long description
+                            """.trimIndent(),
+                    )
+                }
+            },
+        )
     }
 }
