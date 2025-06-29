8676b8c7136d5d2d3eecca1fe33a1561393cf9fe
Maksim <maksvarlyga@gmail.com>
2025-06-29 09:51:16 +0300

    feat(localization): added objects/TemplateKeys.kt

---
+++ app/src/main/kotlin/org/coremapx/app/localization/objects/TemplateKeys.kt
@@ -0,0 +1,9 @@
+package org.coremapx.app.localization.objects
+
+object TemplateKeys {
+    const val ERROR_TYPE = "{errorType}"
+    const val ERROR_DESCRIPTION = "{errorDescription}"
+    const val FILE_NAME = "{fileName}"
+    const val DIRECTORY_PATH = "{directoryPath}"
+    const val FILE_FORMAT = "{fileFormat}"
+}
