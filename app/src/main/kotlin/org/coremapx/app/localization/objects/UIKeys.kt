1318f64ae0916b1e683f681e21f32e8ecd698c0e
Maksim <maksvarlyga@gmail.com>
2025-06-29 09:50:32 +0300

    feat(localization): added objects/UIKeys.kt

---
+++ app/src/main/kotlin/org/coremapx/app/localization/objects/UIKeys.kt
@@ -0,0 +1,28 @@
+package org.coremapx.app.localization.objects
+
+object UIKeys {
+    // Main Menu
+    const val MAIN_MENU_BUTTON_NEW_GRAPH = "mainMenuButtonNewGraph"
+    const val MAIN_MENU_NEW_GRAPH_ICON_DESCRIPTION = "mainMenuNewGraphIconDescription"
+    const val MAIN_MENU_BUTTON_SAVE = "mainMenuButtonSave"
+    const val MAIN_MENU_SAVE_ICON_DESCRIPTION = "mainMenuSaveIconDescription"
+    const val MAIN_MENU_BUTTON_SAVE_AS = "mainMenuButtonSaveAs"
+    const val MAIN_MENU_SAVE_AS_ICON_DESCRIPTION = "mainMenuSaveAsIconDescription"
+    const val MAIN_MENU_BUTTON_OPEN_GRAPH = "mainMenuButtonOpenGraph"
+    const val MAIN_MENU_OPEN_GRAPH_ICON_DESCRIPTION = "mainMenuOpenGraphIconDescription"
+    const val MAIN_MENU_BUTTON_OPEN_REPOSITORY = "mainMenuButtonOpenRepository"
+    const val MAIN_MENU_OPEN_REPOSITORY_ICON_DESCRIPTION = "mainMenuOpenRepositoryIconDescription"
+    const val MAIN_MENU_BUTTON_ANALYTICS = "mainMenuButtonAnalytics"
+    const val MAIN_MENU_ANALYTICS_ICON_DESCRIPTION = "mainMenuAnalyticsIconDescription"
+    const val MAIN_MENU_BUTTON_SETTINGS = "mainMenuButtonSettings"
+    const val MAIN_MENU_SETTINGS_ICON_DESCRIPTION = "mainMenuSettingsIconDescription"
+
+    // User Notification
+    const val USER_NOTIFICATION_SAVE_ERROR = "userNotificationSaveError"
+    const val USER_NOTIFICATION_SAVE_SUCCESS = "userNotificationSaveSuccess"
+    const val USER_NOTIFICATION_SAVE_SUCCESS_MESSAGE = "userNotificationSaveSuccessMessage"
+
+    // Error Strings
+    const val ERROR_BASIC_STRING = "errorBasicString"
+    const val ERROR_NO_DESCRIPTION_MESSAGE = "errorNoDescriptionMessage"
+}
