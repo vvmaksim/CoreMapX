c5ea6a46fff028514230b8bfc5bfb4f51d4051bc
Maksim <maksvarlyga@gmail.com>
2025-06-29 09:51:36 +0300

    feat(localization): added objects/LocalizationFormatter.kt

---
+++ app/src/main/kotlin/org/coremapx/app/localization/objects/LocalizationFormatter.kt
@@ -0,0 +1,23 @@
+package org.coremapx.app.localization.objects
+
+import org.coremapx.app.localization.LocalizationManager
+import org.coremapx.app.localization.objects.TemplateKeys.DIRECTORY_PATH
+import org.coremapx.app.localization.objects.TemplateKeys.ERROR_DESCRIPTION
+import org.coremapx.app.localization.objects.TemplateKeys.ERROR_TYPE
+import org.coremapx.app.localization.objects.TemplateKeys.FILE_FORMAT
+import org.coremapx.app.localization.objects.TemplateKeys.FILE_NAME
+
+object LocalizationFormatter {
+    fun getErrorMessage(startString: String, errorType: String, errorDescription: String?): String {
+        return startString
+            .replace(ERROR_TYPE, errorType)
+            .replace(ERROR_DESCRIPTION, errorDescription ?: LocalizationManager.states.ui.errorNoDescriptionMessage.value)
+    }
+
+    fun getSaveGraphSuccessMessage(startString: String, fileName: String, directoryPath: String, fileFormat: String): String {
+        return startString
+            .replace(FILE_NAME, fileName)
+            .replace(DIRECTORY_PATH, directoryPath)
+            .replace(FILE_FORMAT, fileFormat)
+    }
+}
