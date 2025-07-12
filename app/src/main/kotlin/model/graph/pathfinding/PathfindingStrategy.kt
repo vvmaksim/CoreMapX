2dbb30c46929134885821285d30d0f9217654591
Maksim <maksvarlyga@gmail.com>
2025-07-12 21:37:50 +0300

    feat(model): added PathfindingStrategy interface

---
+++ app/src/main/kotlin/model/graph/pathfinding/PathfindingStrategy.kt
@@ -0,0 +1,13 @@
+package model.graph.pathfinding
+
+import model.graph.contracts.Graph
+import model.result.Result
+
+interface PathfindingStrategy <E: Comparable<E>, V: Comparable<V>> {
+    fun findPath(
+        graph: Graph<E, V>?,
+        start: V,
+        end: V,
+        maxPaths: Int = 1,
+    ): Result<List<List<E>>>
+}
