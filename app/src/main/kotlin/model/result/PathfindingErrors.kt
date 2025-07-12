f1273d05d7fdf6c01c89fae99baae7e37882b82d
Maksim <maksvarlyga@gmail.com>
2025-07-12 21:38:33 +0300

    feat(result): added PathfindingErrors.kt

---
+++ app/src/main/kotlin/model/result/PathfindingErrors.kt
@@ -0,0 +1,27 @@
+package model.result
+
+sealed class PathfindingErrors(
+    override val type: String,
+    override val description: String? = null,
+) : Error {
+    data class EmptyGraph(
+        val message: String = "Graph is empty",
+    ) : PathfindingErrors(
+        type = "EmptyGraph",
+        description = message,
+    )
+
+    data class VertexNotFound(
+        val vertexId: Long,
+    ) : PathfindingErrors(
+        type = "VertexNotFound",
+        description = "Vertex with id:${vertexId} not found",
+    )
+
+    data class NoPathFound(
+        val message: String = "No path found",
+    ) : PathfindingErrors(
+        type = "NoPathFound",
+        description = message,
+    )
+}
