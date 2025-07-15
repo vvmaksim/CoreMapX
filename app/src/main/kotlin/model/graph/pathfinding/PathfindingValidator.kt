b3e84579fccea2ec5b6ef221420a1b0ffcf3d450
Maksim <maksvarlyga@gmail.com>
2025-07-15 19:19:55 +0300

    feat(model): added validateInputParameters() function in PathfindingValidator.kt

--- app/src/main/kotlin/model/graph/pathfinding/PathfindingValidator.kt
+++ app/src/main/kotlin/model/graph/pathfinding/PathfindingValidator.kt
@@ -17,5 +17,61 @@ class PathfindingValidator <E: Comparable<E>, V: Comparable<V>> {
             if (!graph.vertices.containsKey(end)) return Result.Error(PathfindingErrors.VertexNotFound(end as Long))
             return Result.Success(true)
         }
+
+        fun <V : Comparable<V>> validateInputParameters(
+            startId: Long?,
+            endId: Long?,
+            maxPaths: Int?,
+            isVertexExist: (V) -> Boolean?,
+            onStartError: () -> Unit,
+            onEndError: () -> Unit,
+            onMaxPathsError: () -> Unit,
+            onSetErrorMessage: (String) -> Unit,
+        ) {
+            if (startId == null || endId == null) {
+                if (startId == null) onStartError()
+                if (endId == null) onEndError()
+                onSetErrorMessage("Vertex id must be Long type")
+                return
+            }
+
+            if (maxPaths == null) {
+                onMaxPathsError()
+                onSetErrorMessage("Max paths must be Int type")
+                return
+            }
+
+            if (maxPaths < 1) {
+                onMaxPathsError()
+                onSetErrorMessage("Max paths must be positive integer")
+                return
+            }
+
+            val startExist = isVertexExist(startId as V)
+            val endExist = isVertexExist(endId as V)
+
+            if (startExist == null || endExist == null) {
+                onStartError()
+                onEndError()
+                onSetErrorMessage("Graph not found")
+                return
+            }
+            if (!startExist && !endExist) {
+                onStartError()
+                onEndError()
+                onSetErrorMessage("Vertices with $startId and $endId ids not found in graph")
+                return
+            }
+            if (!startExist) {
+                onStartError()
+                onSetErrorMessage("Vertex with $startId id not found in graph")
+                return
+            }
+            if (startExist && !endExist) {
+                onEndError()
+                onSetErrorMessage("Vertex with $endId id not found in graph")
+                return
+            }
+        }
     }
 }
