19db181d6c5c64fcbfe55ff34e8b7be22c0ff769
Maksim <maksvarlyga@gmail.com>
2025-07-29 00:54:27 +0300

    feat(tests): added CanvasManagerTests.kt

---
+++ app/src/test/kotlin/viewmodel/CanvasManagerTests.kt
@@ -0,0 +1,49 @@
+package viewmodel
+
+import org.coremapx.app.userDirectory.UserDirectory
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import viewmodel.managers.CanvasManager
+import kotlin.test.assertEquals
+
+class CanvasManagerTests {
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setupConfig() {
+            UserDirectory.initTestEnvironment(
+                testPath = System.getProperty("java.io.tmpdir") + "/coremapx_canvas_manager_test_" + System.currentTimeMillis(),
+            )
+        }
+    }
+
+    val defaultCanvasLimit = 8000
+    lateinit var canvasManager: CanvasManager
+
+    @BeforeEach
+    fun setup() {
+        canvasManager = CanvasManager()
+    }
+
+    @Test
+    fun `reset canvas`() {
+        assertEquals(0F, canvasManager.offsetX.value)
+        assertEquals(0F, canvasManager.offsetY.value)
+        assertEquals(1F, canvasManager.scale.value)
+        canvasManager.moveCanvas(dx = 3F, dy = 52F)
+        assertEquals(3F, canvasManager.offsetX.value)
+        assertEquals(52F, canvasManager.offsetY.value)
+        canvasManager.zoomCanvas(delta = 2F)
+        assertEquals(2F, canvasManager.scale.value)
+        canvasManager.resetCanvas()
+        assertEquals(0F, canvasManager.offsetX.value)
+        assertEquals(0F, canvasManager.offsetY.value)
+        assertEquals(1F, canvasManager.scale.value)
+    }
+
+    @Test
+    fun `get canvasLimit`() {
+        assertEquals(defaultCanvasLimit, canvasManager.canvasLimit)
+    }
+}
