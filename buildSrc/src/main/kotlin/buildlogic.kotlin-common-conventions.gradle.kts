2c22b17e90ae8fe8050ff26702f92e521d9b48b5
Maksim <maksvarlyga@gmail.com>
2025-08-06 19:19:11 +0300

    feat(buildSrc): added dependencies for ui tests

--- buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-common-conventions.gradle.kts
@@ -19,9 +19,21 @@ dependencies {
     constraints {
         implementation("org.apache.commons:commons-text:1.11.0")
     }
+
+    // JUnit 5
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
+
+    // JUnit 4 dependencies for Compose UI tests
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.compose.ui:ui-test-junit4:1.6.10")
+
+    // JUnit Vintage Engine to run JUnit 4 tests on JUnit Platform
+    testImplementation("org.junit.vintage:junit-vintage-engine:5.10.1")
+
+    // MockK
     testImplementation("io.mockk:mockk:1.13.10")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java {
